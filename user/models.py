f22abf55fafd2cb0e4a92ad1a7f9be716c72d279
FernandoNarvaez1904 <dfrn2003@gmail.com>
2022-01-03 17:06:32 -0600

    Required email, lastname, first name

--- user/models.py
+++ user/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    email = models.EmailField(blank=False, null=False, unique=True)
+    first_name = models.CharField(max_length=150, blank=False, null=False)
+    last_name = models.CharField(max_length=150, blank=False, null=False)
