82855e3224ccdfaaea8b93d22ec7bf5c8960088e
FernandoNarvaez1904 <dfrn2003@gmail.com>
2022-01-03 11:57:40 -0600

    run_server for hypercorn.
    The autoreload function does not work with hypercorn. Therefore, I implemented it myself. This script will be used to run the server in development.

--- run_server.py
+++ run_server.py
@@ -1,33 +1,36 @@
-import hypercorn.run
-from hypercorn.config import Config
-from social_media_backend.asgi import application as app
 import asyncio
+
 from hypercorn.asyncio import serve
+from hypercorn.config import Config
 from watchgod import awatch, DefaultWatcher
 
-config = Config()
-config.worker_class = "uvloop"
+from social_media_backend.asgi import application as app
 
+
+# Custom Watcher is needed to avoid files on venv
+# Files on venv are avoided because they take too long to load
 class CustomWatcher(DefaultWatcher):
 
     def __init__(self, root_path: str):
         self.ignored_dirs.add("venv")
         super().__init__(root_path)
 
-async def start_new_loop(prev_loop = None, restart=False):
-    if restart:
-        prev_loop.close()
 
-    new_loop = loop = asyncio.get_event_loop()
-    new_loop.set_debug(True)
-    return new_loop
-
-
-async def main(loop):
+# This function will be awaited and when resolved it will shut down the app
+async def watch_changes():
+    # If any source changes happen: breaking loop and returning
     async for changes in awatch('.', watcher_cls=CustomWatcher):
-        loop.run_until_complete(start_new_loop(loop, True))
-
-loop = start_new_loop()
-asyncio.run(main(loop))
+        break
+    return True
 
 
+# These are the configuration of the app
+config = Config()
+config.worker_class = "uvloop"
+
+while True:
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+    loop.run_until_complete(
+        serve(app, config, shutdown_trigger=watch_changes)
+    )
