9cab849c0731f02f64af5252ed94ffa62dc446ba
FernandoNarvaez1904 <dfrn2003@gmail.com>
2022-01-05 15:08:46 -0600

    Added field to query existing friends via the API

--- user/api/types.py
+++ user/api/types.py
@@ -1,7 +1,19 @@
+from typing import List
+
 import strawberry
 import strawberry_django
 from django.contrib.auth import get_user_model
 from strawberry.django import auto
+
+
+@strawberry_django.filter(get_user_model(), lookups=True)
+class UserFilter:
+    username: auto
+    email: auto
+    first_name: auto
+    last_name: auto
+    last_login: auto
+    is_active: auto
 
 
 @strawberry.django.type(get_user_model())
@@ -12,6 +24,7 @@ class User:
     last_name: auto
     last_login: auto
     is_active: auto
+    friends: List["User"] = strawberry.django.field(filters=UserFilter)
 
 
 @strawberry.django.input(get_user_model())
