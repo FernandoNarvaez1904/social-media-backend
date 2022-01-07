d6535ee5e9c6ffab9b2a395e9ac3ef953106f09f
FernandoNarvaez1904 <dfrn2003@gmail.com>
2022-01-07 16:26:54 -0600

    Adding cache to schema

--- social_media_backend/api/schema.py
+++ social_media_backend/api/schema.py
@@ -1,6 +1,7 @@
 import strawberry
+from strawberry.extensions import ParserCache, ValidationCache
 
 from social_media_backend.api.mutation import Mutation
 from social_media_backend.api.query import Query
 
-schema = strawberry.Schema(Query, Mutation)
+schema = strawberry.Schema(Query, Mutation, extensions=[ParserCache(), ValidationCache()])
