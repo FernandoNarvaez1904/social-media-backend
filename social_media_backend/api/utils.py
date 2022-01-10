914003b103df12735a9132be95dfe696c295e793
FernandoNarvaez1904 <dfrn2003@gmail.com>
2022-01-10 15:55:56 -0600

    CREATED clean input decorator
    Deletes all unset values in an input type

--- social_media_backend/api/utils.py
+++ social_media_backend/api/utils.py
@@ -17,6 +17,21 @@ def profile_on_terminal_decorator(func):
     return wrapper
 
 
+# Deletes all unset values in an input type
+def clean_input_decorator(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        for val in kwargs.values():
+            if hasattr(val, "_django_type") and val._django_type.is_input:
+                for key, field in val.__dict__.copy().items():
+                    if not field:
+                        delattr(val, key)
+        r = await func(*args, **kwargs)
+        return r
+
+    return wrapper
+
+
 async def get_lazy_query_set_as_list(query_set: QuerySet) -> List:
     list_coroutine = sync_to_async(list)
     return await list_coroutine(query_set)
