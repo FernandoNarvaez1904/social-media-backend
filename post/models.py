a67b2eafc755c0262e570ad9121335686548ed45
FernandoNarvaez1904 <dfrn2003@gmail.com>
2022-01-10 15:59:58 -0600

    CHANGED publication_date
    publication_date now has a default of now

--- post/models.py
+++ post/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 # Create your models here.
 class Post(models.Model):
     description = models.TextField()
-    publication_date = models.DateTimeField()
+    publication_date = models.DateTimeField(default=now, blank=True)
     creation_date = models.DateTimeField(auto_now=True)
-
