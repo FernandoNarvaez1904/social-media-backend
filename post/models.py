7552597c1d75560d17d929a8a04ae57db3370cc3
FernandoNarvaez1904 <dfrn2003@gmail.com>
2022-01-10 14:16:33 -0600

    CREATED intial Post Model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    description = models.TextField()
+    publication_date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now=True)
+
