2d8aa76e0fc5b21fd066daaeb4ec2943309b9434
FernandoNarvaez1904 <dfrn2003@gmail.com>
2022-01-10 16:00:45 -0600

    CREATED CreatePostInput

---
+++ post/api/input.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+import strawberry_django
+from strawberry.django import auto
+from strawberry.schema.types.base_scalars import DateTime
+
+from post.models import Post
+
+
+@strawberry_django.input(Post)
+class CreatePostInput:
+    description: auto
+    publication_date: Optional[DateTime]
