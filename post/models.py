48d9d668658bc3e65cddb1ab1a84422d9013657f
FernandoNarvaez1904 <dfrn2003@gmail.com>
2022-01-10 16:28:44 -0600

    ADDED user field in post

--- post/models.py
+++ post/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.utils.timezone import now
 
-
 # Create your models here.
+from user.models import User
+
+
 class Post(models.Model):
     description = models.TextField()
     publication_date = models.DateTimeField(default=now, blank=True)
     creation_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, related_name="my_posts", on_delete=models.PROTECT)
