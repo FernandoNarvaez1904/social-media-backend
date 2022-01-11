1e0882ca9dac4dc3857c1ed5a7602360c81caa05
FernandoNarvaez1904 <dfrn2003@gmail.com>
2022-01-11 09:13:57 -0600

    CREATED create_comment_mutation

--- post/api/mutations.py
+++ post/api/mutations.py
@@ -3,8 +3,8 @@ import strawberry_django
 from asgiref.sync import sync_to_async
 from strawberry.types import Info
 
-from post.api.input import CreatePostInput, DeletePostInput
-from post.api.types import PostType
+from post.api.input import CreatePostInput, DeletePostInput, CreateCommentInput
+from post.api.types import PostType, CommentType
 from social_media_backend.api.utils import clean_input_decorator, get_lazy_query_set_as_list
 from user.api.utils import login_required_decorator
 from user.models import User
@@ -30,3 +30,10 @@ class Mutation:
             raise Exception(f"Post {data.id} does not exist")
         await sync_to_async(post[0].delete)()
         return True
+
+    @strawberry_django.field
+    @login_required_decorator
+    async def create_comment(self, info: Info, data: CreateCommentInput) -> CommentType:
+        user: User = info.variable_values.get("user")
+        post = await sync_to_async(user.comments.create)(**data.__dict__)
+        return post
