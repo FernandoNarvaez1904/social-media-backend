f4f6ae88d88bef623c31b76d396e1499f5110b81
diegoromero <yolos12345>
2022-04-07 01:13:08 -0600

    Sending not recieved messages when opening sub

--- messages_social/api/subscription.py
+++ messages_social/api/subscription.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import strawberry
 from asgiref.sync import sync_to_async
 from strawberry.types import Info
@@ -5,6 +7,7 @@ from strawberry_django_plus import gql
 
 from messages_social.api.types import MessagesType
 from messages_social.models import Messages
+from social_media_backend.api.utils import get_lazy_query_set_as_list
 from user.models import User
 
 
@@ -12,8 +15,21 @@ from user.models import User
 class Subscription:
 
     @strawberry.subscription
-    async def my_messages(self, info: Info) -> MessagesType:
+    async def my_messages(self, info: Info) -> List[MessagesType]:
+
         user: User = info.context.get("user")
+
+        not_received_messages = await get_lazy_query_set_as_list(
+            Messages.objects.filter(received=False, receiver_id=user.id))
+        if not_received_messages:
+            for message in not_received_messages:
+                message.received = True
+                await sync_to_async(message.save)()
+            yield not_received_messages
+
         async with info.context.broadcast.subscribe(channel=f"chatroom-{user.id}") as subscriber:
             async for event in subscriber:
-                yield sync_to_async(Messages.objects.get)(pk=event.message)
+                message: Messages = await sync_to_async(Messages.objects.get)(pk=event.message)
+                message.received = True
+                await sync_to_async(message.save)()
+                yield [message]
